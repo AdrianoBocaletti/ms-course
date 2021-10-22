b80212b50d642226df7d1dcd91d98fe072f01368
Adriano Bocaletti <adriano-asb@hotmail.com>
2021-10-22 01:29:57 -0300

    hr-payroll implementation mock

--- hr-payroll/src/main/java/com/example/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/HrPayrollApplication.java
@@ -1,4 +1,4 @@
-package com.example.payroll;
+package com.example.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
