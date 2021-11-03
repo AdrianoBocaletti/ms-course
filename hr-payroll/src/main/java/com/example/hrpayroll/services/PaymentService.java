76d2f96846fa04ce891737895fae18dce0784450
Adriano Bocaletti <adriano-asb@hotmail.com>
2021-11-03 20:32:29 -0300

    Feign Client

--- hr-payroll/src/main/java/com/example/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/services/PaymentService.java
@@ -1,37 +1,44 @@
 package com.example.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
+/*Imports quando utilizado RestTemplate*/
+//import java.util.HashMap;
+//import java.util.Map;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.web.client.RestTemplate;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.example.hrpayroll.entities.Payment;
 import com.example.hrpayroll.entities.Worker;
+import com.example.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
+	
+// Quando utilizava RestTemplate
+//	  @Value("${hr-worker.host}")
+// 	  private String workerHost;	
+//	  @Autowired
+//	  private RestTemplate restTemplate;
 	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
+	
 	
 	public Payment getPayment(Long workerId, int days) {
 		
 		//return new Payment("Bob", 200.00, days); // retorno feito sem referenciar o projeto hr-worker
 		
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", workerId.toString());
+		/*Utilizando RestTemplate*/
+//		Map<String, String> uriVariables = new HashMap<>();
+//		uriVariables.put("id", workerId.toString());
+//		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", //URL 
+//				  Worker.class,                 //Tipo do objeto buscado
+//				  uriVariables); 				//Parametros
 		
-		
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", //URL 
-												  Worker.class,                 //Tipo do objeto buscado
-												  uriVariables); 				//Parametros
-		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 		
 	}
